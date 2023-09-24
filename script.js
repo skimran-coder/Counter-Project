012418bdbd82eff5524ed884d8f672a045ae3c93
Sk. Imran Hussain <skimranpc@gmail.com>
2023-09-25 00:00:53 +0530

    Add Counter Project

---
+++ script.js
@@ -0,0 +1,18 @@
+let counter = document.getElementById('count');
+
+function decrement() {
+    value = parseInt(counter.innerText);
+    value = value - 1;
+    counter.innerText = value;
+};
+
+let increment = ()  => {
+    value = parseInt(counter.innerText);
+    value = value + 1;
+    counter.innerText = value;
+}
+
+
+
+
+
